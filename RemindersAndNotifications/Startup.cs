d5ac8a76689525f0640e15b356791573ef7a30ea
Pavan Teja <pavanit1217@gmail.com>
2015-08-20 12:45:21 +0530

    Initial Commit

---
+++ RemindersAndNotifications/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+
+namespace RemindersAndNotifications
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var hubConfiguration = new HubConfiguration();
+            hubConfiguration.EnableDetailedErrors = true; 
+            app.MapSignalR();
+        }
+    }
+}
